0f2f9959f9ff431fcbc806c66869462994053354
LendefiMarkets <alexei@lendefimarkets.com>
2025-12-07 14:25:27 -0800

    chore: remove Infura references, use only Alchemy for network URLs

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,6 @@ require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x" + "0".repeat(64);
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID || "";
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -36,16 +35,12 @@ module.exports = {
       chainId: 31337,
     },
     sepolia: {
-      url: INFURA_PROJECT_ID 
-        ? `https://sepolia.infura.io/v3/${INFURA_PROJECT_ID}`
-        : `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [PRIVATE_KEY],
       chainId: 11155111,
     },
     mainnet: {
-      url: INFURA_PROJECT_ID
-        ? `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`
-        : `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [PRIVATE_KEY],
       chainId: 1,
     },
